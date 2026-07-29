36d79ef248e71e4d6290f354ff72fded9207bd9c
Ajay Saini <ajaynxt2004@gmail.com>
2026-07-29 12:12:03 +0530

    Delete site-config.js

--- site-config.js
+++
@@ -1,7 +0,0 @@
-// AJAYNXT final client choice. Change these values and commit to permanently lock a selection.
-window.IRONPULSE_CONFIG = {
-  theme: 'orange', // orange | lime | red | blue
-  copy: 'sales',   // sales | premium | community
-  lockTheme: false,
-  lockCopy: false
-};
